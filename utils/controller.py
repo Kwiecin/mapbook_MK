0d36a89b6bc503394fbb8733146c7bc1db654bef
Kwiecin <mati.esl@wp.pl>
2025-04-11 09:34:22 +0200

    wip: simple data structure upgraded

--- utils/controller.py
+++ utils/controller.py
@@ -1,5 +1,6 @@
+def get_user_info(users_data:list) -> None:
 for user in users_data:
     print(f"twój znajomy  {user['name']} z miejscowości {user['location']} opublikował {user['posts']} postów")
 
 
-def get_user_info(users_data:list) -> None:
+
