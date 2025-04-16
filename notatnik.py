73f32b588b775ab4436f1693ce0d39d842ef6861
Kwiecin <mati.esl@wp.pl>
2025-04-16 10:56:37 +0200

    wip: new function: add new user

--- notatnik.py
+++ notatnik.py
@@ -1,6 +1,17 @@
-while True:
-    day:str=input("jaki dziś dzień")
-    if day=="środa":
-        print("brawo zgadłeś")
-        break
-    print("nie zgadłeś spróbuj jeszcze raz ")
+users: list = [
+    {"name": "Mateusz", "location": "Morąg", "posts": 0},
+
+]
+print(users)
+
+
+def add_user(users_data: list) -> None:
+    user_name = input("podaj imię nowego użytkowanika: ")
+    user_location = input("podaj lokalizację nowego znajomego: ")
+    user_posts = int(input("podaj liczbę postów znajomego: "))
+    users_data.append({"name": user_name, "location": user_location, "posts": user_posts})
+
+
+add_user(users)
+
+print(users)
--- utils/controller.py
+++ utils/controller.py
@@ -1,6 +1,18 @@
-def get_user_info(users_data:list) -> None:
-for user in users_data:
-    print(f"twój znajomy  {user['name']} z miejscowości {user['location']} opublikował {user['posts']} postów")
+def get_user_info(users_data: list) -> None:
+    for user in users_data:
+        print(f"twój znajomy  {user['name']} z miejscowości {user['location']} opublikował {user['posts']} postów")
+    users: list = [
+        {"name": "Mateusz", "location": "Morąg", "posts": 0},
 
+    ]
+    print(users)
 
+    def add_user(users_data: list) -> None:
+        user_name = input("podaj imię nowego użytkowanika: ")
+        user_location = input("podaj lokalizację nowego znajomego: ")
+        user_posts = int(input("podaj liczbę postów znajomego: "))
+        users_data.append({"name": user_name, "location": user_location, "posts": user_posts})
 
+    add_user(users)
+
+    print(users)
